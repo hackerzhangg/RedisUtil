3b52f99421f0e686391ee3e7d552971a90c52624
zgp <2088220905@qq.com>
2021-07-20 17:20:52 +0800

    redisutil

---
+++ src/main/java/com/bigjava18/redisutil/RedisutilApplication.java
@@ -0,0 +1,13 @@
+package com.bigjava18.redisutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisutilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisutilApplication.class, args);
+    }
+
+}
